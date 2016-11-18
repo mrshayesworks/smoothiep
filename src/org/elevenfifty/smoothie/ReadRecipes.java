d3522a4064994ec9c9cc26ddd3b0a5b1a433d6f7
Danielle <mrshayesworks@gmail.com>
2016-11-18 00:02:07 -0500

    first commit

---
+++ src/org/elevenfifty/smoothie/ReadRecipes.java
@@ -0,0 +1,81 @@
+package org.elevenfifty.smoothie;
+
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+import org.elevenfifty.smoothie.fruit.Banana;
+import org.elevenfifty.smoothie.fruit.Strawberry;
+import org.elevenfifty.smoothie.other.Rum;
+
+public class ReadRecipes {
+
+	public static void main(String[] args) {
+		ReadRecipes readRecipes = new ReadRecipes();
+
+		final Collection<Recipe> recipes = readRecipes.loadRecipes("recipes.csv");
+
+		System.out.println(recipes);
+	}
+
+	public Collection<Recipe> loadRecipes(String filename) {
+		final File recipeFile = new File(filename);
+		final Collection<Recipe> recipes = new ArrayList<>();
+
+		final InputStream recipeStream;
+		try {
+			recipeStream = new FileInputStream(recipeFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't find the file: " + recipeFile.getAbsolutePath());
+			return recipes;
+		}
+
+		try (Scanner input = new Scanner(recipeStream)) {
+			while (input.hasNextLine()) {
+				String[] items = input.nextLine().split(",");
+				recipes.add(buildRecipe(items));
+			}
+		}
+		return recipes;
+	}
+
+	public Recipe buildRecipe(String[] items) {
+		Recipe recipe = new Recipe(items[0]);
+
+		for (int i = 1; i < items.length; i++) {
+			recipe.addIngredient(createIngredient(items[i]));
+		}
+
+		return recipe;
+	}
+
+	public Ingredient createIngredient(String ingredientName) {
+		if (equalsIgnoreCase("Banana", ingredientName)) {
+			return new Banana();
+		} else if ("Strawberry".equalsIgnoreCase(ingredientName)) {
+			return new Strawberry();
+		} else if("Rum".equalsIgnoreCase(ingredientName)) {
+			return new Rum();
+		}
+//
+//		switch (fruitName.toLowerCase()) {
+//		case "banana": //NOTE: This is lower case!
+//			return new Banana();
+//		case "strawberry":
+//			return new Strawberry();
+//		}
+//		// ADVANCED OPTION (Cannot 100% implement here)
+//		new HashMap<String, Fruit>();
+//		return fruitMap.get(fruitName.toLowerCase());
+
+		return null;
+	}
+
+}
